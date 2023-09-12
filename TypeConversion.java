a9ebdf34d0bdd42bbdb5b38a4fac5ccc5aa6b97a
Omkar tonde <111636424+tondeomkar@users.noreply.github.com>
2023-09-12 13:12:58 +0530

    Add files via upload

---
+++ TypeConversion.java
@@ -0,0 +1,10 @@
+//TypeConversions
+public class TypeConversion 
+{
+    public static void main(String[] arg)
+    {
+        int a = 10;
+        long b = a;
+        System.out.println(b);
+    }
+}
