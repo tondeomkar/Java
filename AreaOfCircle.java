174e9d1ed16373ad6f83642b0124c15a63824a4a
Omkar tonde <111636424+tondeomkar@users.noreply.github.com>
2023-10-08 20:18:56 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class AreaOfCircle
+{
+    public static void main(String a [])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        float rad = sc.nextFloat();
+
+        float area = 3.14f * rad * rad;
+        System.out.println(area);
+    }
+}
